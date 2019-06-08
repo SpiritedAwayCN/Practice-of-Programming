21e758469206f2625b8883bd19cb8d509e8c2ac3
SpiritedAwayCN <541845219@qq.com>
2019-06-08 12:31:36 +0800

    dp

---
+++ noi-openjudge/2.7基本算法之算法效率/神奇的口袋.cpp
@@ -0,0 +1,17 @@
+﻿#include<cstdio>
+constexpr int MOD = 10000;
+int dp[45], n, a[25];
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &a[i]);
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++)
+		for (int j = 40; j >= a[i]; j--) {
+			dp[j] += dp[j - a[i]];
+			//dp[j] %= MOD;
+		}
+	printf("%d", dp[40]);
+	return 0;
+}
