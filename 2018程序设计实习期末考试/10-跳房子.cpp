a2f0250530d5adcfccb28b1f790db999546e1830
SpiritedAwayCN <541845219@qq.com>
2019-06-17 16:33:17 +0800

    2018-final

---
+++ 2018程序设计实习期末考试/10-跳房子.cpp
@@ -0,0 +1,56 @@
+﻿#include<cstdio>
+#include<string>
+#include<iostream>
+#include<cstring>
+#include<map>
+#include<queue>
+#include<algorithm>
+using namespace std;
+struct node {
+	long long number;
+	int step;
+};
+map<long long, long long> vis;
+queue<node> BFS_queue;
+long long s, t;
+int cnt;
+string ans;
+inline bool push_queue(long long num, long long pred, int step) {
+	//25步以内*3不会溢出
+	if (vis.find(num) != vis.end()) return false;
+	vis.insert(make_pair(num, pred));
+	if (num == t) {
+		cnt = step + 1;
+		return true;
+	}
+	BFS_queue.push(node{ num,++step });
+	return false;
+}
+
+int main() {
+	while (scanf("%lld%lld", &s, &t) != EOF) {
+		if (s == 0 && t == 0) break;
+		vis.clear();
+		ans.clear();
+		while (!BFS_queue.empty()) BFS_queue.pop();
+
+		BFS_queue.push(node{ s,0 });
+		vis.insert(make_pair(s, -1));
+		while (!BFS_queue.empty()) {
+			node num = BFS_queue.front();
+			BFS_queue.pop();
+			if (push_queue(num.number * 3, num.number, num.step)) break;
+			if (push_queue(num.number >> 1, num.number, num.step)) break;
+
+		}
+		long long last = t, pred;
+		while (last != s) {
+			pred = vis.at(last);
+			ans += pred > last ? 'O' : 'H';
+			last = pred;
+		}
+		reverse(ans.begin(), ans.end());
+		cout << cnt << '\n' << ans << endl;
+	}
+	return 0;
+}
---
+++ 2018程序设计实习期末考试/7-神奇的数列.cpp
@@ -0,0 +1,36 @@
+﻿#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+constexpr int MAXN = 205;
+constexpr int INF = 0x7fffffff;
+int dp[MAXN][MAXN], a[MAXN], n;
+
+int get_dp(int s, int t) {
+	if (dp[s][t] != -1) return dp[s][t];
+	int temp = INF;
+	for (int i = s; i < t; i++)
+		temp = min(temp, get_dp(s, i) + get_dp(i + 1, t));
+	if (a[s] == a[t])
+		temp = min(temp, get_dp(s, t - 1));
+	return dp[s][t] = temp;
+}
+
+int main() {
+	int T;
+	scanf("%d", &T);
+	for (int cntT = 1; cntT <= T; cntT++) {
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++)
+			scanf("%d", &a[i]);
+		memset(dp, -1, sizeof(dp));
+		for (int i = 1; i <= n; i++)
+			dp[i][i] = 1;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j < i; j++)
+				dp[i][j] = 0;
+		printf("Case %d: %d\n", cntT, get_dp(1, n));
+	}
+	return 0;
+}
