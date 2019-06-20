56404bfb8365fd3fe673a3227679ed504197fe88
SpiritedAwayCN <541845219@qq.com>
2019-06-20 12:05:59 +0800

    Exercise

---
+++ 2019程序设计实习0526上机/古代密码.cpp
@@ -0,0 +1,30 @@
+﻿#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+
+char str1[105], str2[105];
+int cont1[30], cont2[30];
+int main() {
+	bool flag = false;
+	scanf("%s", str1);
+	scanf("%s", str2);
+	int l1 = strlen(str1), l2 = strlen(str2);
+	if (l1 == l2) {
+		for (int i = 0; i < l1; i++) {
+			cont1[str1[i] - 'A']++;
+			cont2[str2[i] - 'A']++;
+		}
+		sort(cont1, cont1 + 26);
+		sort(cont2, cont2 + 26);
+		flag = true;
+		for (int i = 0; i < 26; i++)
+			if (cont1[i] != cont2[i]) flag = false;
+	}
+	if (flag)
+		printf("YES");
+	else
+		printf("NO");
+	return 0;
+}
---
+++ noi-openjudge/2.5基本算法之搜索/放苹果.cpp
@@ -0,0 +1,28 @@
+﻿#include<cstdio>
+int ans;
+void dfs(int n, int m, int l) {
+	if (m == 1 || n == 0) {
+		if (l >= n) {
+			ans++;
+		}
+		return;
+	}
+	if (l > n) l = n;
+	for (int i = l; i > 0; i--) {
+		dfs(n - i, m - 1, i);
+	}
+		
+}
+
+int main() {
+	int T;
+	int n, m;
+	scanf("%d", &T);
+	while (T--) {
+		ans = 0;
+		scanf("%d%d", &n, &m);
+		dfs(n, m, n);
+		printf("%d\n", ans);
+	}
+	return 0;
+}
